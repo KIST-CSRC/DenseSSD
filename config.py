829ebb4cfdd5377e4a477e5d0da19ff2ee7cb3a2
yoomambo <yoohj9475@naver.com>
2022-11-17 14:57:26 +0900

    [Mod] add data path and hyperparameters

--- config.py
+++ config.py
@@ -2,9 +2,18 @@
 device = "cuda:0"
 
 # Path to data dir
-image_dir = 'dataset/test_sample'
+test_sample_image_dir = 'dataset/test_sample'
+test_result_image_dir = 'dataset/test_result'
+image_dir_train = 'dataset/training_sample/Empty/img'
+info_dir_train  = 'dataset/training_sample/Empty/annotation'
+train_label     = 'train.txt'
+val_label       = 'valid.txt'
 
 # Hyper parameters
 model_path = 'pre-trained/model.pth'
 image_size = 300
 C = 3
+batch_size      = 1
+init_lr         = 0.001
+weight_decay    = 5e-4
+num_epochs      = 5
